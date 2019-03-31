59c21281d82ba1b689cd35fa6c1fff1d598b8d8f
Jessica Richards <jessbodie@gmail.com>
2019-03-31 15:45:44 -0400

    Palindrome exercise - input word and check if palindrome

---
+++ palindrome.js
@@ -0,0 +1,21 @@
+document.getElementById('palBtn').addEventListener('click', function () {
+    palindromeChecker(document.getElementById('palInput').value);
+});
+document.getElementById('palInput').addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
+        palindromeChecker(e.target.value);
+    }
+});
+
+function palindromeChecker (pal) {
+    pal =((pal.toLowerCase()).split('')).filter(p => p !== ' ');
+    for (let i = 0; i < (pal.length/2); i++) {
+        if (pal[i] === pal[pal.length - i - 1]) {
+            palindromeStatus = true;
+        } else {
+            alert('Sorry, this is not a palindrome');
+            return;
+        }
+    }
+    alert('This is a palindrome!')
+}
